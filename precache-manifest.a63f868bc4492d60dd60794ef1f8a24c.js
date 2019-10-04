154db60c6366fa260692173bcaf3e1880fa71be5
Derek J. Augustine <augshow@gmail.com>
2019-10-04 10:10:25 -0500

    Updates

---
+++ precache-manifest.a63f868bc4492d60dd60794ef1f8a24c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5b17095e0a7a1ccc7ebc3b27e7c6ab8",
+    "url": "/raffletron-2k19/index.html"
+  },
+  {
+    "revision": "e3ff5297069bc7cf018f",
+    "url": "/raffletron-2k19/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5e9a65e59ac500c478a8",
+    "url": "/raffletron-2k19/static/js/2.57720050.chunk.js"
+  },
+  {
+    "revision": "e3ff5297069bc7cf018f",
+    "url": "/raffletron-2k19/static/js/main.ec69344d.chunk.js"
+  },
+  {
+    "revision": "5292ee426b3f68e7ffec",
+    "url": "/raffletron-2k19/static/js/runtime-main.c179815e.js"
+  },
+  {
+    "revision": "8bdf1bcc001d01495a93f1543ef88799",
+    "url": "/raffletron-2k19/static/media/Ralph.8bdf1bcc.gif"
+  },
+  {
+    "revision": "516831cd39beeca1ff1d4f48642ea955",
+    "url": "/raffletron-2k19/static/media/RalphSr.516831cd.gif"
+  },
+  {
+    "revision": "e29bc649f111ed330d8f3aced1dc3f2d",
+    "url": "/raffletron-2k19/static/media/Ralphette.e29bc649.gif"
+  }
+]);
